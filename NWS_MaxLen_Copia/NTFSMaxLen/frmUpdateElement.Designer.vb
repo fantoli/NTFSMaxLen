f59b9808a89924aa3ccf7b008e9ad2d41c9443c7
MrTolins <ferantoli22@gmail.com>
2021-09-27 17:07:07 +0200

    First Commit

---
+++ NWS_MaxLen_Copia/NTFSMaxLen/frmUpdateElement.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUpdateElement
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.RadLabel2 = New Telerik.WinControls.UI.RadLabel()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btnAceptar = New Telerik.WinControls.UI.RadButton()
+        Me.btnCancelar = New Telerik.WinControls.UI.RadButton()
+        Me.txtName = New Telerik.WinControls.UI.RadTextBox()
+        Me.lblPathLen = New Telerik.WinControls.UI.RadLabel()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel3.SuspendLayout()
+        CType(Me.btnAceptar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnCancelar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtName, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.lblPathLen, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.AutoSize = True
+        Me.TableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.RadLabel2, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel3, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.txtName, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPathLen, 1, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(349, 91)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'RadLabel2
+        '
+        Me.RadLabel2.Location = New System.Drawing.Point(3, 3)
+        Me.RadLabel2.Name = "RadLabel2"
+        Me.RadLabel2.Size = New System.Drawing.Size(48, 18)
+        Me.RadLabel2.TabIndex = 0
+        Me.RadLabel2.Text = "Nombre"
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.TableLayoutPanel3.AutoSize = True
+        Me.TableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel3.ColumnCount = 2
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.btnAceptar, 0, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.btnCancelar, 1, 0)
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(146, 55)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 1
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(200, 30)
+        Me.TableLayoutPanel3.TabIndex = 3
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.Location = New System.Drawing.Point(3, 3)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(94, 24)
+        Me.btnAceptar.TabIndex = 2
+        Me.btnAceptar.Text = "Aceptar"
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancelar.Location = New System.Drawing.Point(103, 3)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(94, 24)
+        Me.btnCancelar.TabIndex = 3
+        Me.btnCancelar.Text = "Cancelar"
+        '
+        'txtName
+        '
+        Me.txtName.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtName.Location = New System.Drawing.Point(57, 3)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(289, 20)
+        Me.txtName.TabIndex = 0
+        '
+        'lblPathLen
+        '
+        Me.lblPathLen.Location = New System.Drawing.Point(57, 29)
+        Me.lblPathLen.Name = "lblPathLen"
+        Me.lblPathLen.Size = New System.Drawing.Size(58, 18)
+        Me.lblPathLen.TabIndex = 4
+        Me.lblPathLen.Text = "RadLabel1"
+        '
+        'frmUpdateElement
+        '
+        Me.AcceptButton = Me.btnAceptar
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.CancelButton = Me.btnCancelar
+        Me.ClientSize = New System.Drawing.Size(349, 91)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmUpdateElement"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "frmUpdateElement"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        CType(Me.btnAceptar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnCancelar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtName, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.lblPathLen, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents RadLabel2 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents TableLayoutPanel3 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents btnAceptar As Telerik.WinControls.UI.RadButton
+    Friend WithEvents btnCancelar As Telerik.WinControls.UI.RadButton
+    Friend WithEvents txtName As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents lblPathLen As Telerik.WinControls.UI.RadLabel
+End Class
